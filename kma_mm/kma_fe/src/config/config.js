e235c5ff1a81d6f2aed19d53c02e6ef7692d0b34
Nhanchinh <thannhanchinh2472003@gmail.com>
2025-01-19 22:58:40 +0700

    setup axios+login cứng :>

---
+++ kma_mm/kma_fe/src/config/config.js
@@ -0,0 +1,8 @@
+const config = {
+    baseUrl: "https://example.com/api", // URL cơ bản của server
+    timeout: 10000,                    // Thời gian chờ tối đa
+    environment: process.env.NODE_ENV, // Môi trường (development, production)
+    apiVersion: "v1",                  // Phiên bản API (nếu cần)
+};
+
+export default config;
