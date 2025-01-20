88f54f606418823d4c32c88c132357661ac562f4
PC <zuoq2110@gmail.com>
2025-01-20 10:39:58 +0700

    create student dashboard

--- kma_mm/kma_fe/src/App.jsx
+++ kma_mm/kma_fe/src/App.jsx
@@ -1,19 +1,66 @@
-// src/App.jsx
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
+import { BrowserRouter as Router, Routes, Route, Navigate } from "react-router-dom";
+import Login from "./components/Login/Login";
 import Dashboard from "./components/Dashboard/Dashboard";
-import Login from "./components/login/Login";
+import AdminDashboard from "./components/Dashboard/AdminDashboard";
+import StudentDashboard from "./components/Dashboard/StudentDashboard";
+import PrivateRoute from "./setPermiss/PrivateRoute";
+import AdminManage from "./components/manage/AdminManage";
 
 const App = () => {
-  const [role, setRole] = useState("");
+  // Lấy role từ localStorage khi khởi động
+  const [role, setRole] = useState(localStorage.getItem("role") || "");
+
+  // Hàm xử lý đăng nhập (set role và lưu vào localStorage)
+  const handleLogin = (role) => {
+    setRole(role);
+    localStorage.setItem("role", role); // Lưu role vào localStorage
+  };
 
   return (
-    <div>
-      {!role ? (
-        <Login onLogin={(role) => setRole(role)} />
-      ) : (
-        <Dashboard role={role} />
-      )}
-    </div>
+    <Router>
+      <Routes>
+        {/* Login Route */}
+        <Route
+          path="/login"
+          element={
+            !role ? (
+              <Login onLogin={handleLogin} />
+            ) : (
+              <Navigate to={`/${role}/dashboard`} />
+            )
+          }
+        />
+
+        {/* Protected Routes */}
+        <Route
+          path="/admin/dashboard"
+          element={
+            <PrivateRoute role={role} allowedRoles={["admin"]}>
+              <AdminDashboard />
+            </PrivateRoute>
+          }
+        />
+        {/* Thêm route admin/manage */}
+        <Route
+          path="/admin/manage"
+          element={
+            <PrivateRoute role={role} allowedRoles={["admin"]}>
+              <AdminManage /> {/* Hiển thị component admin manage */}
+            </PrivateRoute>
+          }
+        />
+        <Route
+          path="/sv/dashboard"
+          element={
+            <PrivateRoute role={role} allowedRoles={["sv"]}>
+              <StudentDashboard />
+            </PrivateRoute>
+          }
+        />
+        <Route path="*" element={<Navigate to="/login" />} />
+      </Routes>
+    </Router>
   );
 };
 
---
+++ kma_mm/kma_fe/src/components/manage/AdminManage.jsx
@@ -0,0 +1,10 @@
+// components/Dashboard/AdminManage.jsx
+import React from "react";
+import { Link } from "react-router-dom";
+const AdminManage = () => {
+    return <div>Admin Manage Page
+        <Link to="/login">Logout</Link>
+    </div>;
+};
+
+export default AdminManage;
