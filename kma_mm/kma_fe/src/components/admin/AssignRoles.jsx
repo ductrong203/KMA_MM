d6eceee22281635dc517ae13b2e5dd56d382e9de
Nhanchinh <thannhanchinh2472003@gmail.com>
2025-01-21 21:32:36 +0700

    admin add user

--- kma_mm/kma_fe/src/components/admin/AssignRoles.jsx
+++ kma_mm/kma_fe/src/components/admin/AssignRoles.jsx
@@ -1,12 +1,21 @@
 import React, { useState } from 'react';
-import { Button, TextField, Grid, Typography } from '@mui/material';
+import { Button, TextField, Grid, Typography, MenuItem } from '@mui/material';
 
 const AssignRoles = () => {
     const [username, setUsername] = useState('');
     const [role, setRole] = useState('');
 
+    const roleMapping = {
+        1: "training",
+        2: "examination",
+        3: "student_manage",
+        4: "library",
+        5: "director",
+        6: "sv",
+        7: "admin",
+    };
+
     const handleAssignRole = () => {
-        // Mock dữ liệu
         console.log(`Assigning role ${role} to user ${username}`);
         alert(`Role ${role} assigned to ${username}`);
     };
@@ -26,12 +35,19 @@ const AssignRoles = () => {
                 </Grid>
                 <Grid item xs={12} sm={6}>
                     <TextField
+                        select
                         label="Role"
                         variant="outlined"
                         fullWidth
                         value={role}
-                        onChange={(e) => setRole(e.target.value)}
-                    />
+                        onChange={(e) => setRole(parseInt(e.target.value))} // Nếu muốn giữ role dạng số
+                    >
+                        {Object.entries(roleMapping).map(([key, value]) => (
+                            <MenuItem key={key} value={parseInt(key)}>
+                                {value}
+                            </MenuItem>
+                        ))}
+                    </TextField>
                 </Grid>
                 <Grid item xs={12}>
                     <Button variant="contained" color="primary" onClick={handleAssignRole}>
