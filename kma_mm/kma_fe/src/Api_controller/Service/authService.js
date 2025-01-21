2474e12a2eff1cf3fdd72bdac5b52d158171c15c
Nhanchinh <thannhanchinh2472003@gmail.com>
2025-01-21 15:54:35 +0700

    login register phân quyền

--- kma_mm/kma_fe/src/Api_controller/Service/authService.js
+++ kma_mm/kma_fe/src/Api_controller/Service/authService.js
@@ -5,16 +5,28 @@ export const login = async (username, password) => {
     if (!username || !password) {
         throw new Error("Please enter both username and password.");
     }
-
     const response = await api.post("/auth/login", { username, password });
     const { access_token, data } = response.data;
-
+    // Bảng ánh xạ role từ API sang tên vai trò
+    const roleMapping = {
+        1: "training",
+        2: "examination",
+        3: "student_manage",
+        4: "library",
+        5: "director",
+        6: "sv",
+        7: "admin",
+    };
+    // Lấy tên role từ roleMapping
+    const roleName = roleMapping[data.role];
+    if (!roleName) {
+        throw new Error("Role không hợp lệ."); // Xử lý lỗi nếu role không xác định
+    }
     // Lưu token và thông tin người dùng vào localStorage
     localStorage.setItem("access_token", access_token);
-    localStorage.setItem("role", data.role === 1 ? "training" : "sv");
+    localStorage.setItem("role", roleName);
     localStorage.setItem("username", data.username);
-
-    return data.role === 1 ? "training" : "sv"; // Trả về role để sử dụng
+    return roleName; // Trả về role để sử dụng
 };
 
 // Hàm xử lý đăng ký
