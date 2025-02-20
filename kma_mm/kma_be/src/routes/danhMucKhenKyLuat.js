41dda6cef8ab4995c21f3e500ffa9e989816d1c3
ductrong <trongdeck@gmail.com>
2025-02-20 07:35:42 +0700

    phan quyen cho he quan ly hoc vien them sua xoa khen thuong ky luat

--- kma_mm/kma_be/src/routes/danhMucKhenKyLuat.js
+++ kma_mm/kma_be/src/routes/danhMucKhenKyLuat.js
@@ -1,12 +1,32 @@
 const express = require("express");
 const DanhMucKhenKyLuatController = require("../controllers/danhMucKhenKyLuatController");
-
+const { authQuanLyHocVienMiddleWare } = require("../middelWare/authMiddelWare");
 const router = express.Router();
 
-router.post("/", DanhMucKhenKyLuatController.create);
-router.get("/", DanhMucKhenKyLuatController.getAll);
-router.get("/:id", DanhMucKhenKyLuatController.getById);
-router.put("/:id", DanhMucKhenKyLuatController.update);
-router.delete("/:id", DanhMucKhenKyLuatController.delete);
+router.post(
+  "/",
+  authQuanLyHocVienMiddleWare,
+  DanhMucKhenKyLuatController.create
+);
+router.get(
+  "/",
+  authQuanLyHocVienMiddleWare,
+  DanhMucKhenKyLuatController.getAll
+);
+router.get(
+  "/:id",
+  authQuanLyHocVienMiddleWare,
+  DanhMucKhenKyLuatController.getById
+);
+router.put(
+  "/:id",
+  authQuanLyHocVienMiddleWare,
+  DanhMucKhenKyLuatController.update
+);
+router.delete(
+  "/:id",
+  authQuanLyHocVienMiddleWare,
+  DanhMucKhenKyLuatController.delete
+);
 
 module.exports = router;
