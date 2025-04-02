ef920bab540e4fa75a27e001df71b2163da551f3
PC <zuoq2110@gmail.com>
2025-04-02 18:34:17 +0700

    update

--- kma_mm/kma_be/src/routes/keHoachMonHoc.js
+++ kma_mm/kma_be/src/routes/keHoachMonHoc.js
@@ -3,6 +3,7 @@ const router = express.Router();
 const KeHoachMonHocController = require('../controllers/keHoachMonHocController');
 
 router.get("/getbykhoavaky/:khoa_dao_tao_id/:ky_hoc?", KeHoachMonHocController.getByKhoaDaoTaoAndKyHoc);
+router.post("/getbykhoavaky", KeHoachMonHocController.getMHByKhoaDaoTaoAndKyHoc);
 router.post("/monhoc", KeHoachMonHocController.getMonHocByKhoaDaoTaoAndKyHoc);
 router.get('/', KeHoachMonHocController.getAll);
 router.get('/:id', KeHoachMonHocController.getById);
