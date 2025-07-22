9b2b578a13a6c3da2b1adee312e14019ec8a210b
duong <zuoq2110@gmail.com>
2025-07-22 19:48:49 +0700

    update

--- kma_mm/kma_be/src/config/config.js
+++ kma_mm/kma_be/src/config/config.js
@@ -1,23 +1,17 @@
 const dotenv = require("dotenv");
-const path = require("path");
 dotenv.config();
-
-// Load .env file từ thư mục gốc (1 cấp trên src)
-dotenv.config({ path: path.resolve(__dirname, "../../.env") });
-console.log("DB_PASSWORD:", process.env.DB_PASSWORD);
 module.exports = {
   development: {
     username: process.env.DB_USERNAME || "root",
-    password: process.env.DB_PASSWORD || "secret",
-    database: process.env.DB_DATABASE || "quan_ly_dao_tao",
-    host: process.env.DB_HOST || "hn-fornix-testing-bigdata-5",
-    port: process.env.PORT || "3301",
+    password: process.env.DB_PASSWORD || "",
+    database: process.env.DB_DATABASE || "quan_ly_dao_tao2",
+    host: process.env.DB_HOST || "127.0.0.1",
     dialect: process.env.DB_DIALECT || "mysql",
   },
   test: {
     username: "root",
-    password: "secret",
-    database: "quan_ly_dao_tao",
+    password: null,
+    database: "database_test",
     host: "127.0.0.1",
     dialect: "mysql",
   },
