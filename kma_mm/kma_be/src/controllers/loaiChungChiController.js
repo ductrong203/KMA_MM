e502b29aa330916f5b346eec3ae97b6eb9126abc
itsTranT1706 <khiemtrangia60@gmail.com>
2025-08-18 22:37:35 +0700

    feat[logs]: chung chỉ & loai chung chi (create+update+del)

--- kma_mm/kma_be/src/controllers/loaiChungChiController.js
+++ kma_mm/kma_be/src/controllers/loaiChungChiController.js
@@ -1,4 +1,20 @@
 const LoaiChungChiService = require('../services/loaiChungChiService');
+const { loai_chung_chi } = require('../models');
+
+const { logActivity } = require("../services/activityLogService");
+const { getFieldById } = require("../utils/detailData");
+const { users } = require("../models");
+const { getDiffData } = require("../utils/getDiffData");
+const { verifyAccessToken } = require("../utils/decodedToken");
+const mapRole = {
+  1: "daoTao",
+  2: "khaoThi",
+  3: "quanLiSinhVien",
+  5: "giamDoc",
+  6: "sinhVien",
+  7: "admin"
+
+}
 
 class LoaiChungChiController {
   /**
@@ -8,9 +24,9 @@ class LoaiChungChiController {
   static async layDanhSachLoaiChungChi(req, res) {
     try {
       const { tinh_trang } = req.query;
-      
+
       const result = await LoaiChungChiService.layDanhSachLoaiChungChi(tinh_trang);
-      
+
       return res.status(200).json({
         success: true,
         message: 'Lấy danh sách loại chứng chỉ thành công',
@@ -33,7 +49,7 @@ class LoaiChungChiController {
   static async layChiTietLoaiChungChi(req, res) {
     try {
       const { id } = req.params;
-      
+
       if (!id || isNaN(parseInt(id))) {
         return res.status(400).json({
           success: false,
@@ -43,7 +59,7 @@ class LoaiChungChiController {
       }
 
       const result = await LoaiChungChiService.layChiTietLoaiChungChi(parseInt(id));
-      
+
       return res.status(200).json({
         success: true,
         message: 'Lấy chi tiết loại chứng chỉ thành công',
@@ -67,9 +83,33 @@ class LoaiChungChiController {
   static async taoLoaiChungChi(req, res) {
     try {
       const data = req.body;
-      
+
       const result = await LoaiChungChiService.taoLoaiChungChi(data);
-      
+      try {
+        const token = req.headers.authorization?.split(" ")[1];
+        // console.log(token);
+        let user = verifyAccessToken(token);
+        let userN = await getFieldById("users", user.id, "username");
+        let userR = await getFieldById("users", user.id, "role");
+
+        if (result) {
+          let inforActivity = {
+            username: userN,
+            role: mapRole[userR],
+            action: req.method,
+            endpoint: req.originalUrl,
+            reqData: `Người dùng ${userN} đã tạo thành công chứng chỉ ${req.body.ten_loai_chung_chi} ${req.body.xet_tot_nghiep == false ? "không xét tốt nghiệp" : "xét tốt nghiệp"}`,
+            response_status: 200,
+            resData: "Tạo loại chứng chỉ thành công",
+            ip: req._remoteAddress,
+
+          }
+          await logActivity(inforActivity);
+        }
+      } catch (error) {
+        console.error("Lỗi kìa ní:", error.message);
+      }
+
       return res.status(201).json({
         success: true,
         message: 'Tạo loại chứng chỉ thành công',
@@ -94,7 +134,7 @@ class LoaiChungChiController {
     try {
       const { id } = req.params;
       const data = req.body;
-      
+
       if (!id || isNaN(parseInt(id))) {
         return res.status(400).json({
           success: false,
@@ -102,9 +142,45 @@ class LoaiChungChiController {
           data: null,
         });
       }
-
+      const oldDataRaw = await loai_chung_chi.findByPk(id);
+      const oldData = {
+        "Loại chứng chỉ": oldDataRaw.ten_loai_chung_chi,
+        "Mô tả": oldDataRaw.mo_ta === null ? "chưa có mô tả": oldDataRaw.mo_ta  ,
+        "Xét tốt nghiệp": oldDataRaw.xet_tot_nghiep === false ? "có" : "không",
+        "Tình trạng": oldDataRaw.tinh_trang,
+      }
+      // console.log("$$#####",oldDataRaw);
       const result = await LoaiChungChiService.capNhatLoaiChungChi(parseInt(id), data);
-      
+      try {
+        const token = req.headers.authorization?.split(" ")[1];
+        // console.log(token);
+        let user = verifyAccessToken(token);
+        let userN = await getFieldById("users", user.id, "username");
+        let userR = await getFieldById("users", user.id, "role");
+        if (result) {
+          const newDataRaw = await loai_chung_chi.findByPk(id);
+          const newData = {
+            "Loại chứng chỉ": newDataRaw.ten_loai_chung_chi,
+            "Mô tả": newDataRaw.mo_ta === null ? "chưa có mô tả": newDataRaw.mo_ta  ,
+            "Xét tốt nghiệp": newDataRaw.xet_tot_nghiep === false ? "có" : "không",
+            "Tình trạng": newDataRaw.tinh_trang,
+          }
+          let inforActivity = {
+            username: userN,
+            role: mapRole[userR],
+            action: req.method,
+            endpoint: req.originalUrl,
+            reqData: `${getDiffData(oldData, newData)}`,
+            response_status: 200,
+            resData: `Người dùng ${userN} đã cập nhật loại chứng chỉ ${data.ten_loai_chung_chi} `,
+            ip: req._remoteAddress,
+
+          }
+          await logActivity(inforActivity);
+        }
+      } catch (error) {
+        console.error("Lỗi kìa ní:", error.message);
+      }
       return res.status(200).json({
         success: true,
         message: 'Cập nhật loại chứng chỉ thành công',
@@ -133,7 +209,7 @@ class LoaiChungChiController {
   static async xoaLoaiChungChi(req, res) {
     try {
       const { id } = req.params;
-      
+
       if (!id || isNaN(parseInt(id))) {
         return res.status(400).json({
           success: false,
@@ -141,9 +217,32 @@ class LoaiChungChiController {
           data: null,
         });
       }
-
+      let loaiChungChi = await getFieldById("loai_chung_chi", id, "ten_loai_chung_chi");
       const result = await LoaiChungChiService.xoaLoaiChungChi(parseInt(id));
-      
+      try {
+        const token = req.headers.authorization?.split(" ")[1];
+        // console.log(token);
+        let user = verifyAccessToken(token);
+        let userN = await getFieldById("users", user.id, "username");
+        let userR = await getFieldById("users", user.id, "role");
+
+        if (result) {
+          let inforActivity = {
+            username: userN,
+            role: mapRole[userR],
+            action: req.method,
+            endpoint: req.originalUrl,
+            reqData: `Người dùng ${userN}  đã xóa thành công loại chứng chỉ ${loaiChungChi}`,
+            response_status: 200,
+            resData: "Xóa loại chứng chỉ thành công",
+            ip: req._remoteAddress,
+
+          }
+          await logActivity(inforActivity);
+        }
+      } catch (error) {
+        console.error("Lỗi kìa ní:", error.message);
+      }
       return res.status(200).json({
         success: true,
         message: 'Xóa loại chứng chỉ thành công',
@@ -172,7 +271,7 @@ class LoaiChungChiController {
   static async timKiemLoaiChungChi(req, res) {
     try {
       const { tu_khoa } = req.query;
-      
+
       if (!tu_khoa) {
         return res.status(400).json({
           success: false,
@@ -182,7 +281,7 @@ class LoaiChungChiController {
       }
 
       const result = await LoaiChungChiService.timKiemLoaiChungChi(tu_khoa);
-      
+
       return res.status(200).json({
         success: true,
         message: 'Tìm kiếm loại chứng chỉ thành công',
@@ -206,7 +305,7 @@ class LoaiChungChiController {
     try {
       const { id } = req.params;
       const { tinh_trang } = req.body;
-      
+
       if (!id || isNaN(parseInt(id))) {
         return res.status(400).json({
           success: false,
@@ -216,7 +315,7 @@ class LoaiChungChiController {
       }
 
       const result = await LoaiChungChiService.thayDoiTrangThaiLoaiChungChi(parseInt(id), tinh_trang);
-      
+
       return res.status(200).json({
         success: true,
         message: 'Thay đổi trạng thái loại chứng chỉ thành công',
