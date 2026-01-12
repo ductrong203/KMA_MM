190a5b9af5adcc84ee6a1d9b630b8b7c9a2b510f
duong <zuoq2110@gmail.com>
2026-01-13 00:08:18 +0700

    update

--- kma_mm/kma_be/src/config/config.js
+++ kma_mm/kma_be/src/config/config.js
@@ -9,8 +9,7 @@ module.exports = {
     username: process.env.DB_USERNAME || "root",
     password: process.env.DB_PASSWORD || "",
     database: process.env.DB_DATABASE || "quan_ly_dao_tao2",
-    host: process.env.DB_HOST || "hn-fornix-testing-bigdata-5",
-    port: process.env.PORT || "3301",
+    host: process.env.DB_HOST || "127.0.0.1",
     dialect: process.env.DB_DIALECT || "mysql",
   },
   test: {
